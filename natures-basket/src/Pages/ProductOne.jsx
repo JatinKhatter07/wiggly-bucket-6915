ee49bc59e03b90b4822788be7ec584f6a1a5a336
Priyankadubey190 <priyankadubey0811@gmail.com>
2022-11-09 20:42:24 +0530

    day2 commit

---
+++ natures-basket/src/Pages/ProductOne.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Products from '../components/Products'
+
+export const ProductOne = () => {
+  return (
+    <div>
+        <Products />
+    </div>
+  )
+}
---
+++ natures-basket/src/images/product.js
@@ -0,0 +1,209 @@
+export const productone = [
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/f7eb0f83-c578-4f72-a181-7e54094f0193_425x425.jpg",
+        title: "BLUEBERRIES COMBO",
+        des1: "BLUEBERRIES COMBO",
+        des2: "",
+        qty: "1 Pc",
+        price: 629
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/70726a6d-f35e-480c-ba42-c95e808e3ec1_425x425.jpg",
+        title: "Apple Red Delicious - Washington",
+        des1: "Product of : USA.",
+        des2: "When it comes to apples, our mind remembers Washington automatically. Enjoy every bite of this All time favourite apples of the world.",
+        qty: "500 g",
+        price: 224.5
+    }, 
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/f341c972-bc85-4b0d-8309-6bdaa4fd0cad_425x425.JPG",
+        title: "Grapes Red Globe - Imported",
+        des1: "Product of : USA.",
+        des2: "Red Globe grapes are mildly sweet and flavourful. They are not as tangy as Flame seedless",
+        qty: "200 g",
+        price: 99.8
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/620b0bb1-440e-4d83-896a-35bc6f274c3c_425x425.jpg",
+        title: "Apple Fuji",
+        des1: "Product of : USA.",
+        des2: "A distinctive, honey sweet taste and a juicy texture makes this apple delectable",
+        qty: "500 g",
+        price: 214.5
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/d2f0e000-0d5f-4153-a8c7-caddb93bb9eb_425x425.jpg",
+        title: "FRESHCON SHREDDED COCONUT 100G",
+        des1: "Product of : INDIA.",
+        des2: "",
+        qty: "1 Pc",
+        price: 40
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/b8473f37-a863-4a02-8bc5-81c523f5290e_425x425.jpg",
+        title: "YUMMIEZ GREEN PEAS 500g",
+        des1: "Product of : INDIA.",
+        des2: "",
+        qty: "1 Pc",
+        price: 125
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/8ddeaf63-7d49-4498-bfd8-2dc80e3df458_425x425.JPG",
+        title: "Nectarine - Imported",
+        des1: "Product of : Spain.",
+        des2: "Juicy nectarine has a delicious flavor that can be relished as is or in desserts",
+        qty: "1 kg",
+        price: 2199
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/52f5fdaa-c9c5-4026-bf57-5e2bf06c24cf_425x425.JPG",
+        title: "Orange",
+        des1: "Product of : INDIA.",
+        des2: "Full of soluble fiber, tangy, juicy oranges are helpful in lowering cholesterol",
+        qty: "1 kg",
+        price: 129
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/e052813e-041b-44ee-b255-b7356c6af4ac_425x425.JPG",
+        title: "Green Peas",
+        des1: "Product of : INDIA.",
+        des2: "Sweet, delicious, and cutely round, green peas are also called garden peas.",
+        qty: "1 kg",
+        price: 359
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/ced65f9e-c6b2-41fc-9662-7c5a564e5c2b_425x425.jpg",
+        title: "Apple New Zealand Rose",
+        des1: "Product of : New Zealand/USA.",
+        des2: "Here's the pefect rich and pulpy apples which are developed and bred in New Zealand",
+        qty: "1 kg",
+        price: 214.5
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/63b69966-4855-4b1f-80a2-74796c8a5e09_425x425.JPG",
+        title: "Custard Apple",
+        des1: "Product of : INDIA.",
+        des2: "A popular fruit, the custard apple is much loved globally for its unique shape and sweet taste.",
+        qty: "1 kg",
+        price: 74.75
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/Images/no-images425x425.jpg",
+        title: "PEARS BEAUTY IMPORTED",
+        des1: "PEARS BEAUTY IMPORTED",
+        des2: "",
+        qty: "1 kg",
+        price: 189.5
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/7fb83bef-4c1c-4f81-96e6-44c4d3eac077_425x425.jpg",
+        title: "Apple Royal Gala - New Zealand",
+        des1: "Product of : New Zealand/USA.",
+        des2: "Royal Gala was developed in New Zealand and is a red sport of the Gala variety.",
+        qty: "1 kg",
+        price: 174.5
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/c952f42a-21b8-4271-98ba-58e0afdd47bb_425x425.jpg",
+        title: "PEARS PACKHAM IMPORTED",
+        des1: "PEARS PACKHAM",
+        des2: "",
+        qty: "1 kg",
+        price: 299
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/fb35012c-ed68-4553-af94-b44c35ed8586_425x425.jpg",
+        title: "Apple Green",
+        des1: "Product of : New Zealand/USA.",
+        des2: "The compound quercitin, a major component in green apple peels, helps to lower sugar levels and manage type-2 diabetes mellitus. ",
+        qty: "1 kg",
+        price: 199.5
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/6fe7cc7b-c825-4126-90da-1c631719940c_425x425.JPG",
+        title: "NB SPINACH 200G",
+        des1: "Product of : INDIA",
+        des2: "",
+        qty: "1 Pc",
+        price: 55
+    }
+
+
+]
+
+export const producttwo = [
+    {
+    img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/01623b62-0190-483e-8d05-f260c3adc3cc_425x425.jpg",
+    title: "NB ORGANIC CHEESE PLATTER",
+    des1: "ORGANIC GOUDA, FLAVOURED CHEESE - LIKE THYME, CHILLI, BASIL, PEPPER",
+    des2: "",
+    qty: "1 Pc",
+    price: 2500
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/76cb8a9c-d7b2-4f74-aaf4-e96c92538757_425x425.jpg",
+        title: "MEAT PLATTER Chicken & Pork - 799",
+        des1: "MEAT PLATTER Chicken & Pork - 799",
+        des2:"",
+        qty:"1 Pc",
+        price: 799
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/ecb76305-cf41-4ef5-b34f-f9969746ccb4_425x425.jpg",
+        title: "MEAT PLATTER Chicken - 299",
+        des1: "MEAT PLATTER Chicken - 299",
+        des2:"",
+        qty:"1 Pc",
+        price:  299
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/2d775da9-bf34-40f2-a64a-f68d419cf2e6_425x425.jpg",
+        title: "MEAT PLATTER Pork - 499",
+        des1: "MEAT PLATTER Pork - 499",
+        des2:"",
+        qty:"1 Pc",
+        price: 499
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/a0b9487f-e09f-48af-9908-d1d805cc66f5_425x425.jpg",
+        title: "MEAT PLATTER Chicken, Turkey, Pork - 899",
+        des1: "MEAT PLATTER Chicken, Turkey, Pork - 899",
+        des2:"",
+        qty:"1 Pc",
+        price: 899
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/e9712c32-8a4b-47de-a716-f3ade788c01c_425x425.jpg",
+        title: "MEAT PLATTER Pork - 699",
+        des1: "MEAT PLATTER Pork - 699",
+        des2:"",
+        qty:"1 Pc",
+        price: 699
+    },
+
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/ba3898e3-11dd-4a8b-836b-7dc5a53b9590_425x425.jpg",
+        title: "MEAT PLATTER Chicken - 199",
+        des1: "MEAT PLATTER Chicken - 199",
+        des2:"",
+        qty:"1 Pc",
+        price: 199
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/0a0f4359-ce08-40a7-88a5-c9bc8f7d12cb_425x425.jpg",
+        title: "MEAT PLATTER Chicken & Turkey - 699",
+        des1: "MEAT PLATTER Chicken & Turkey - 699",
+        des2:"",
+        qty:"1 Pc",
+        price: 699
+    },
+    {
+        img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/28808ca7-cef1-4afd-884d-c243d46dac89_425x425.jpg",
+        title: "MEAT PLATTER Turkey - 599",
+        des1: "MEAT PLATTER Turkey - 599",
+        des2:"",
+        qty:"1 Pc",
+        price: 599
+    }
+
+]
